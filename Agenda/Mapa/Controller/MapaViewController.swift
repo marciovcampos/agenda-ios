694d4a276d4912409d90eb5c7a63fa442952d5c5
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-24 19:34:51 -0300

    Mapa - mudando titulo

--- Agenda/Mapa/Controller/MapaViewController.swift
+++ Agenda/Mapa/Controller/MapaViewController.swift
@@ -12,19 +12,12 @@ class MapaViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = getTitulo()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getTitulo() -> String {
+        return "Localizar Alunos"
     }
-    */
+
 
 }
