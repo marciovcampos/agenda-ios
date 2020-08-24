e5f48db076481c5cba35c4f42834205c040382f0
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-24 19:37:23 -0300

    Mapa - passando o aluno para a tela do Mapa

--- Agenda/Home/Controller/HomeTableViewController.swift
+++ Agenda/Home/Controller/HomeTableViewController.swift
@@ -76,6 +76,7 @@ class HomeTableViewController: UITableViewController, UISearchBarDelegate, NSFet
                     
                 case .mapa:
                     let mapa = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mapa") as! MapaViewController
+                    mapa.aluno = alunoSelecionado
                     self.navigationController?.pushViewController(mapa, animated: true)
                     break
                     
--- Agenda/Mapa/Controller/MapaViewController.swift
+++ Agenda/Mapa/Controller/MapaViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class MapaViewController: UIViewController {
+    
+    // MARK: - Variavel
+    
+    var aluno:Aluno?
+    
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = getTitulo()
+        
+        print(aluno?.nome)
     }
     
     func getTitulo() -> String {
